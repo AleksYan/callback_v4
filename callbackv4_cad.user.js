49ff32ac5f27e39ffe20311e288233498f7af8ea
AleksYan <oyankovskyi@gmail.com>
2021-04-12 22:09:25 +0200

    comments

--- callbackv4_cad.user.js
+++ callbackv4_cad.user.js
@@ -2,7 +2,7 @@
 // @name         WxCC_Script_callback_v4_change
 // @namespace    http://tampermonkey.net/
 // @version      1.4
-// @description  helper script to update callbackv4 CADVariablesToInclude value
+// @description  helper script to update CADVariablesToInclude value in custom call script callback_options_to_change_callback_number_multi_lang_v4.js
 // @author       Aleksey Yankovskyy (ayankovs@cisco.com)
 // @match        https://portal.wxcc-eu1.cisco.com/cdsui/rs/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
@@ -31,6 +31,7 @@
         var scriptParams = document.getElementById("controlScriptParamsDiv").getElementsByClassName("form-group");
         var i = scriptParams.length-1
 
+        // predefined value for last call script param. In this case CAD variable value
         var newTextFieldOuterHtml = `<div class="col-sm-4"><input id="controlScriptParam_${i}" type="text" value="CallbackRequested,CustomerName,primarySkill,primaryAgentName" class="form-control"></div>`;
                 
         scriptParams[scriptParams.length-1].childNodes[1].outerHTML = newTextFieldOuterHtml;
